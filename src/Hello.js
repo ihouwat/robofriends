6f120d47061f7a32930ac6098478004f75747d08
MSU PRL <msuprlab@gmail.com>
2020-01-10 17:52:33 -0500

    created test Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -4,12 +4,23 @@ import './Hello.css'
 class Hello extends Component {
   render() {
     return (
-      <div>
+      <div className="f1 tc">
         <h1>Hello World</h1>
-        <p>Welcome to React</p>
+        <p>{this.props.greeting}</p>
       </div>
     );
   }
 }
+
+// This example is to show that the code above works like a function
+// If you comment out the top code and activate this, we get the same result
+// const Hello = (props) => {
+//   return (
+//     <div className="f1 tc">
+//                <h1>Hello World</h1>
+//         <p>{props.greeting}</p>
+//        </div>
+//      );
+// }
 
 export default Hello
