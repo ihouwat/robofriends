3fa7f117de7ea9b76a0b8ede686a701383aa3ff2
MSU PRL <houwat@gmail.com>
2020-03-05 19:08:13 -0500

    Implemented searchField in redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList.js';
 import SearchBox from '../components/SearchBox.js';
 // import { robots } from './robots.js'; //Using destructuring because robots.js doesn't have export 'default'
@@ -6,12 +7,24 @@ import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
 class App extends Component {
   constructor() {
     super() // Super calls the constructor of the component class. 
     this.state = { // State is what describes our App. It can change and affect the app. State lives in the parent component.
       robots: [],
-      searchfield: ''
     }
   }
 
@@ -21,21 +34,18 @@ class App extends Component {
       .then(users => {this.setState({ robots: users})});// takes the empty array from this.state into a list of users fetchec from the website above
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value}) // setState is a method that comes with React. This one resets the state of the searchfield
-  }
-
   render() {
-    const { robots, searchfield} = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props; // Coming from the Redux store (see index.js file)
     const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase()) //If anything in the string includes anything in the string, you return an array with available robots
+      return robot.name.toLowerCase().includes(searchField.toLowerCase()) //If anything in the string includes anything in the string, you return an array with available robots
     })
     return !robots.length ?  //robots.length === 0 evaluates to 'false'. So, we are saying, if NOT false, return the nested code 
       //This is a loading bar component
       <h1>Loading</h1> : 
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots}/>
@@ -45,4 +55,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
