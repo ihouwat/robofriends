de6ca421c96ebbf7eec40acc071a4c3ca04b1137
ihouwat <houwat@gmail.com>
2020-03-18 18:59:52 -0400

    Edited manifest.json PWA - part 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import CounterButton from './CounterButton';
 
 class Header extends Component {
   shouldComponentUpdate(nextProps, nextState) {
@@ -10,7 +9,7 @@ class Header extends Component {
     return (
     <div>
       <h1 className='f1'>RoboFriends</h1>
-      <CounterButton color={'red'} />
+      {/* <CounterButton color={'red'} /> */}
     </div>
     )
   }
