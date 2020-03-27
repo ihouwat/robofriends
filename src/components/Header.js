50aa54281a05a7db77166a8d8d5360927959d75a
ihouwat <houwat@gmail.com>
2020-03-27 19:15:26 -0400

    Setting up Enzyme tests

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
     return (
     <div>
       <h1 className='f1'>RoboFriends</h1>
-      {/* <CounterButton color={'red'} /> */}
+      <CounterButton color={'red'} />
     </div>
     )
   }
